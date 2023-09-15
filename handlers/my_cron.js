ef94f9246eccebf13c856119e77795fdc9a9b406
kishor82 <kishorrathva8298@gmail.com>
2023-09-15 14:54:49 +0530

    feat: added winston logger with sentry.

--- handlers/my_cron.js
+++ handlers/my_cron.js
@@ -1,4 +1,5 @@
+const logger = require('../logger');
 
 module.exports = (message) => {
-    console.log(message);
+    logger.info("Hello World!")
 };
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const cron = require('node-cron');
 const { resolve } = require('path');
 const jobs = require('./jobs');
+const logger = require('./logger');
 
 const initCrons = () => {
+    logger.info("Cron started...")
     Object.keys(jobs).forEach((key) => {
         if (cron.validate(jobs[key].frequency)) {
             cron.schedule(jobs[key].frequency, () => {
